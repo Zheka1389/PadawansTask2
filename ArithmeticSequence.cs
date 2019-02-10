b8513f64f55fb642128259bccf7cc79ed42a193b
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-02-10 22:27:22 +0300

    1

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -7,7 +7,10 @@ namespace PadawansTask2
         public static int Calculate(int number, int add, int count)
         {
             // put your code here
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            int result = (add < 0 || count < 0) ? throw new ArgumentException() :
+                number * count + add * ((count - 1) * count / 2);
+            return result;
         }
     }
 }
